d8a7a723abc1ac397ac78187aac5553a5bbb97c5
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-24 14:36:40 +0600

    review section data send  database

--- src/Pages/Dahboard/AddReview.js
+++ src/Pages/Dahboard/AddReview.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import auth from '../../firebase.init';
 
 const AddReview = () => {
+    const [user] = useAuthState(auth);
+
+    const reviewHandle = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        // const date = event.target.date.value;
+        const stars = event.target.stars.value;
+        const massage = event.target.massage.value;
+        const review = { name, email, stars, massage }
+        console.log(review)
+
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(review),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                alert('Product added');
+                event.target.reset();
+            })
+    }
+
     return (
         <div>
-            <h1>this is add review</h1>
+            <div class="bg-white py-6 sm:py-8 lg:py-12">
+                <div class="max-w-screen-2xl px-4 md:px-8 mx-auto lg:ml-36">
+
+                    <div class="mb-10 md:mb-16">
+                        <h2 class="text-gray-800 text-2xl lg:text-4xl font-bold  mb-4 md:mb-6">Add Your Review Here</h2>
+                    </div>
+                    <form onSubmit={reviewHandle} className="max-w-screen-md grid sm:grid-cols-2 gap-4">
+                        <div>
+                            <label className="inline-block text-gray-800 text-sm sm:text-base mb-2">Your Name*</label>
+                            <input type="text" name="name" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required defaultValue={user.displayName} />
+                        </div>
+                        <div>
+                            <label className="inline-block text-gray-800 text-sm sm:text-base mb-2">Email Id*</label>
+                            <input type="text" name="email" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required defaultValue={user.email} />
+                        </div>
+
+
+
+                        <div>
+                            <label className="inline-block text-gray-800 text-sm sm:text-base mb-2">Stars*</label>
+                            <input type="text" name="stars" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required placeholder='Rate 1 to 5' />
+                        </div>
+                        <div>
+                            <label className="inline-block text-gray-800 text-sm sm:text-base mb-2">Date*</label>
+                            <input type="date" name="date" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
+                        </div>
+
+                        <div className="sm:col-span-2">
+                            <label className="inline-block text-gray-800 text-sm sm:text-base mb-2">review*</label>
+                            <textarea type="massage" name="massage" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2 h-20" required />
+                        </div>
+
+                        <div className="sm:col-span-2 flex justify-between items-center">
+                            <button className="inline-block bg-primary hover:bg-gray-500  focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Send</button>
+
+                            <span className="text-gray-500 text-sm">*Required</span>
+                        </div>
+
+
+                        <p className="text-gray-400 text-xs">By signing up to our newsletter you agree to our <a href="/" className="hover:text-indigo-500 active:text-indigo-600 underline transition duration-100">Privacy Policy</a>.</p>
+                    </form>
+
+
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Dahboard/Dahboard.js
+++ src/Pages/Dahboard/Dahboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 // import auth from '../../firebase.init';
 
 
@@ -12,7 +12,7 @@ const Dashboard = () => {
             <div className="drawer drawer-mobile">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
-                    <h2 className='flex justify-between lg:justify-center lg:items-center lg:text-center  lg:text-lg py-3 px-10 text-black  bg-primary'>Welcome To Your Dashboard !!
+                    <h2 className='lg:hidden flex justify-between lg:justify-center lg:items-center lg:text-center  lg:text-lg py-3 px-10 text-black  bg-primary'>Welcome To Your Dashboard !!
                         <label for="my-drawer-2" className="text-sm drawer-button flex justify-center lg:hidden bg-black text-white px-3 py-1 rounded-lg font-semibold">Menu</label></h2>
                     <Outlet></Outlet>
 
@@ -25,15 +25,12 @@ const Dashboard = () => {
 
 
                         <li><Link to='/dashboard' className=' text-white hover:bg-gray-400 lg:mt-10 '>My Orders</Link></li>
-                        <li><Link to='/dashboard/reviews' className=' text-white hover:bg-gray-400 lg:mt-5 '>Add Review</Link></li>
-
-                        <li><Link to='/dashboard/manage_all_orders' className=' text-white hover:bg-gray-400 lg:mt-5  '>Manage Orders</Link></li>
-
-
-                        <li><Link to='/dashboard/manage_products' className=' text-white hover:bg-gray-400 lg:mt-5'>Manage Products</Link></li>
-                        <li><Link to='/dashboard/add_product' className=' text-white hover:bg-gray-400 lg:mt-5 '>Add Product</Link></li>
-                        <li><Link to='/dashboard/make_admin' className=' text-white hover:bg-gray-400 lg:mt-5 '>Make Admin</Link></li>
-                        <li><Link to='/dashboard/profile' className=' text-white hover:bg-gray-400 lg:mt-5 '>My Profile</Link></li>
+                        <li><NavLink to='/dashboard/reviews' className=' text-white hover:bg-gray-400 lg:mt-5 '>Add Review</NavLink></li>
+                        <li><NavLink to='/dashboard/profile' className=' text-white hover:bg-gray-400 lg:mt-5 '>My Profile</NavLink></li>
+                        <li><NavLink to='/dashboard/manage_all_orders' className=' text-white hover:bg-gray-400 lg:mt-5  '>Manage All Orders</NavLink></li>
+                        <li><NavLink to='/dashboard/add_product' className=' text-white hover:bg-gray-400 lg:mt-5 '>Add Product</NavLink></li>
+                        <li><NavLink to='/dashboard/make_admin' className=' text-white hover:bg-gray-400 lg:mt-5 '>Make Admin</NavLink></li>
+                        <li><NavLink to='/dashboard/manage_products' className=' text-white hover:bg-gray-400 lg:mt-5'>Manage Products</NavLink></li>
 
 
 
