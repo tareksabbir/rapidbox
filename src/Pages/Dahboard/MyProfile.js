22fe399390f9c6c7cdbd9a367baeea291792c979
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-25 13:10:14 +0600

    profile update done

--- src/Pages/Dahboard/MyProfile.js
+++ src/Pages/Dahboard/MyProfile.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const MyProfile = () => {
     const [user] = useAuthState(auth);
-    console.log(user);
+    const { email } = useParams();
+
+    const profileUpdate = event => {
+        event.preventDefault();
+        const displayName = event.target.displayName.value;
+        const gender = event.target.gender.value;
+        const education = event.target.education.value;
+        const email = event.target.email.value;
+        const photoURL = event.target.photoURL.value;
+        const company = event.target.company.value;
+        const about = event.target.about.value;
+
+        const profile = { displayName, gender, education, photoURL, about, company, email }
+        console.log(profile);
+
+
+        const url = `http://localhost:5000/user/${email}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(profile),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                alert('Product added');
+                event.target.reset();
+            })
+    }
+
     return (
         <>
-            <section class="text-gray-600 body-font">
+            <section class="text-black body-font">
                 <div class="container px-5  mx-auto flex flex-col">
-                    <div class="lg:w-5/6 mt-10 mx-auto bg-slate-50">
-                        <div class="flex flex-col sm:flex-row mt-10">
-                            <div class="sm:w-2/4 text-center sm:pr-8 sm:py-8">
+                    <div class="lg:w-5/6 w-full mt-10 mx-auto bg-slate-100 p-14">
+                        <div class="flex flex-col sm:flex-row shadow-2xl ">
+                            <div class="sm:w-3/4 text-center sm:pr-8 sm:py-8">
 
 
                                 <div class="w-35 h-35 rounded-full inline-flex items-center justify-center bg-slate-50">
@@ -39,7 +71,7 @@ const MyProfile = () => {
 
                             </div>
                         </div>
-                        <hr></hr>
+
                     </div>
                 </div>
             </section>
@@ -47,10 +79,10 @@ const MyProfile = () => {
                 <div className="bg-white py-6 sm:py-8 lg:py-6 lg:mb-10 lg:mt-5">
                     <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
 
-                        <form className="max-w-screen-md grid sm:grid-cols-2 gap-4  mx-auto">
+                        <form onSubmit={profileUpdate} className="max-w-screen-md grid sm:grid-cols-2 gap-4  mx-auto">
                             <div>
-                                <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">Admin Name*</label>
-                                <input type="text" name="admin" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required defaultValue={user?.displayName} />
+                                <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">User Name*</label>
+                                <input type="text" name="displayName" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required defaultValue={user?.displayName} />
                             </div>
                             <div>
                                 <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2"> Admin Email*</label>
@@ -58,28 +90,28 @@ const MyProfile = () => {
                             </div>
                             <div>
                                 <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">Gender*</label>
-                                <input type="text" name="name" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
+                                <input type="text" name="gender" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
                             </div>
 
 
                             <div>
                                 <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">Education*</label>
-                                <input type="text" name="price" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
+                                <input type="text" name="education" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
                             </div>
                             <div>
                                 <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">Company Name*</label>
-                                <input type="text" name="minimumOrder" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
+                                <input type="text" name="company" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
                             </div>
 
                             <div >
                                 <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">Current Address*</label>
-                                <input type="text" name="available" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
+                                <input type="text" name="address" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
                             </div>
 
 
                             <div className="sm:col-span-2">
                                 <label className="inline-block text-gray-800 text-sm sm:text-sm mb-2">About Me*</label>
-                                <input type="text" name="description" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
+                                <input type="text" name="about" className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" required />
                             </div>
 
                             <div className="sm:col-span-2">
